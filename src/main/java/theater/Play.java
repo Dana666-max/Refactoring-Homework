2e2f91a86bf90b23bb3b869b4f5d5fc485dd6e7c
danam <danamiz2006@gmail.com>
2025-11-18 15:05:24 -0500

    Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,23 @@
 package theater;
 
-public class Play {
+/**
+ * Represents a play through a class.
+ */
 
-    public String name;
-    public String type;
+public class Play {
+    private final String name;
+    private final String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
