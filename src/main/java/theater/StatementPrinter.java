ce05accaf13ef6f70090c1644b98a86a0252a793
danam <danamiz2006@gmail.com>
2025-11-20 16:05:26 -0500

    Task 2.3

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -27,8 +27,6 @@ public class StatementPrinter {
         final StringBuilder result =
                 new StringBuilder("Statement for " + invoice.getCustomer() + System.lineSeparator());
 
-        final NumberFormat format = NumberFormat.getCurrencyInstance(Locale.US);
-
         for (Performance p : invoice.getPerformances()) {
 
             // add volume credits
@@ -37,14 +35,19 @@ public class StatementPrinter {
             // print line for this order
             result.append(
                     String.format(
-                            "  %s: %s (%s seats)%n", getPlay(p).getName(),
-                            format.format(getThisAmount(p) / Constants.PERCENT_FACTOR), p.getAudience()
+                            "  %s: %s (%s seats)%n", getPlay(p).getName(), usd(getThisAmount(p)), p.getAudience()
                     ));
             totalAmount += getThisAmount(p);
         }
-        result.append(String.format("Amount owed is %s%n", format.format(totalAmount / Constants.PERCENT_FACTOR)));
+        usd(totalAmount);
+        result.append(String.format("Amount owed is %s%n", usd(totalAmount)));
         result.append(String.format("You earned %s credits%n", volumeCredits));
         return result.toString();
+    }
+
+    private static StringBuilder usd(int totalAmount) {
+        return new StringBuilder(String.format(
+                NumberFormat.getCurrencyInstance(Locale.US).format(totalAmount / Constants.PERCENT_FACTOR)));
     }
 
     private int getVolumeCredits(Performance performance) {
